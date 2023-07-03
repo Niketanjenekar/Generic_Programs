1d272ad85f93794656c5e22fff5103df21db2e48
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-07-03 14:17:36 +0530

    [Niketan]Add: Welcome to Find Maximum with Using Generics Programs

---
+++ Generics_Program/Generics_Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("**************Welcome to Find Maximum Using Generics Problems**************");
+
+            Console.ReadLine();
+        }
+    }
+}
