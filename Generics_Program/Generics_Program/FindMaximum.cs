64fe896a65aa455ddb8dec007a1cf240e7c26358
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-07-03 14:35:47 +0530

    [Niketan]Add: Usecase 1 for Comparing Three Integers to Find Maximum

---
+++ Generics_Program/Generics_Program/FindMaximum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Program
+{
+    public class FindMaximum
+    {
+        public void FindingMaximum(int a, int b, int c)
+        {
+            if(a.CompareTo(b) > 0  && a.CompareTo(c) >0)
+            {
+                Console.WriteLine("The First Value " + a + "  is The Greatest Value");
+            }
+            else if(b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                Console.WriteLine("The Second Value " + b + "  is Greatest Value");
+            }
+            else
+            {
+                Console.WriteLine("The Third Value " + c + "  is the Greatest Value");
+            }
+        }
+    }
+}
--- Generics_Program/Generics_Program/Program.cs
+++ Generics_Program/Generics_Program/Program.cs
@@ -12,6 +12,19 @@ namespace Generics_Program
         {
             Console.WriteLine("**************Welcome to Find Maximum Using Generics Problems**************");
 
+            //******************************Day-13 Assignments******************************
+
+            Console.WriteLine("1. Find the Maximum Number Between Three Integers");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            FindMaximum findMaximum = new FindMaximum();
+            switch (option)
+            {
+                case 1:
+                    findMaximum.FindingMaximum(25, 99, 45);
+                    break;
+
+            }
             Console.ReadLine();
         }
     }
